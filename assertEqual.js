965c755a96fc62df9aa7b15b13d0d385ee644ef0
Maheerah Anwar <maheerahanwar@gmail.com>
2024-09-28 17:26:52 -0400

    asserterror

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("hey","hello");
+assertEqual("hi", "hi");
+assertEqual(3,5);
+assertEqual(4,4);
