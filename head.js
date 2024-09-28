1b9da3677c850025c221bd361aaea82c3bc5c720
Maheerah Anwar <maheerahanwar@gmail.com>
2024-09-28 17:56:16 -0400

    implementhead

---
+++ head.js
@@ -0,0 +1,19 @@
+const head = function(arr) {
+  if (arr.length === 0) {
+      return undefined; 
+  }
+  return arr[0]; 
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), "undefined");
