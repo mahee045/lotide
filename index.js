a467951ccebaa79c4d700c34b85d74939861b4f4
Maheerah Anwar <maheerahanwar@gmail.com>
2024-10-31 18:10:36 -0400

    index part 2

--- index.js
+++ index.js
@@ -11,29 +11,24 @@ const eqArrays = require('./eqArrays');
 const eqObjects = require('./eqObjects');
 const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
-const flatten = require('./flatten');
 const letterPositions = require('./letterPositions');
-const map = require('./map');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertArraysEqual:   assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual:   assertObjectsEqual,
-  countLetters:   countLetters,
-  countOnly:   countOnly,
-  eqArrays: eqArrays, 
-  eqObjects: eqObjects,
-  findKey: findKey, 
-  findKeyByValue: findKeyByValue, 
-  flatten: flatten, 
-  letterPositions: letterPositions, 
-  map: map, 
-  takeUntil: takeUntil, 
-  without: without,
-
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  takeUntil,
+  without,
 };
